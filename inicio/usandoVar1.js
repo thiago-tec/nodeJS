57e22d3428ae7c9421b47ff621a739aa58e4f713
Thiago de jesus Cordeiro <thiago.cordeiros@hotmail.com>
2023-06-12 12:53:28 -0300

    var com scopo global

---
+++ inicio/usandoVar1.js
@@ -0,0 +1,17 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+}
+
+//console.log(local)//o var local nao vai estar visivel
