27be9c3455a9f5e94139e0be184888d28e7147de
Thiago de jesus Cordeiro <thiago.cordeiros@hotmail.com>
2023-06-12 18:29:57 -0300

    fix conflict

--- inicio/usandoVar1.js
+++ inicio/usandoVar1.js
@@ -13,8 +13,4 @@ console.log(sera)
 function teste(){
     var local = 123
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> b8a52b547eb68a8fcee43b8921b6aef263842d36
 //console.log(local)//o var local nao vai estar visivel
