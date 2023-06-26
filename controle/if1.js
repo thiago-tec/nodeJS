8d881d53d736a9ab715c4c8ba353081e725eba3f
Thiago de Jesus Cordeiro <64814595+thiago-tec@users.noreply.github.com>
2023-06-26 09:46:56 -0300

    Delete controle directory

--- controle/if1.js
+++
@@ -1,7 +0,0 @@
-function soBoaNoticia(nota){
-    if(nota >= 7){
-        console.log('parabéns !')
-    }
-}
-
-soBoaNoticia(8.0)
--- controle/if2.js
+++
@@ -1,9 +0,0 @@
-const imprimirResultado = function(nota){
-    if(nota >= 7){
-        console.log('parabéns !')
-    }else{
-        console.log('reprovado')
-    }
-}
-
-imprimirResultado(7)
